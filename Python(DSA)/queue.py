b40554ce56d12350376fcbd60805730dae5ae92a
mhotydelacruz-arch <mhotydelacruz@gmail.com>
2026-09-21 17:13:39 +0800

    Clean up queue.py by removing unused code
    Removed commented-out code related to Plant and Climate classes.

--- Python(DSA)/queue.py
+++ Python(DSA)/queue.py
@@ -1,19 +1,19 @@
-# class Plant:
-#  def __init__(self, name, water_need):
-#   self.name = name
-#   self.water_need = water_need
-#   self.health = 100
- 
-# print("Congratulations!, you created a plant!")
+# from collections import deque
 
-# updateResources(water=200, seeds=90, energy=110, hope=80, coins=999)
+# class Climate:
+# 	def __init__ (self):
+# 		self.climate = deque()
+	
+# 	def add_challenge(self, event_name):
+# 		self.climate.append(event_name)
+	
+# 	def process_hazard(self):
+# 		return len (self.climate) == 0
+		
+# q = Climate()
+# q.add_challenge("Storm")
+# q.add_challenge("Floods")
+# q.add_challenge("Drought")
 
-# addPlantsToDropdown([
-# ("Eggplant", "🍆", 20),
-# ("Apples", "🍎", 50),
-# ("Banana", "🍌", 10),
-# ("Melon", "🍈", 30),
-# ("Pineaple", "🍍", 70)
-# ])
-
-# print("Resource Storage and Plant Dropdown is Unlocked!")
+# updateClimateQueue(list(q.climate))
+# print("Climate Queue was ready!")
